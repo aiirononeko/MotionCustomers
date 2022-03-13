ca1c352f50e55210da382527d883b7c3112cddb9
aiirononeko <aiirononeko@gmail.com>
2022-03-13 14:17:01 +0900

    Firebaseの初期設定

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+
+  // Firebase初期化処理
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(const MyApp());
 }
 
